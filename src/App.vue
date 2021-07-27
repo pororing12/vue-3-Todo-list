9be7f74894f8ed76dd7729d48be3926da29c7b75
jwkim12 <jwkim@bluedigm.com>
2021-07-27 21:00:04 +0900

    UIcompleted

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,80 @@
 <template>
-  <h2>To-Do List</h2>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search"
+    >
+    <hr>
+    <TodoSimpleForm @add-todo="addTodo"/>
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다
+    </div>
+
+    <div v-if="!filteredTodos.length">
+      There is noting to display
+    </div>
+
+    <TodoList 
+      :todos="filteredTodos" 
+      @toggle-todo="ToggleTodo"
+      @delete-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
-
+import { ref, computed } from "vue";
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
+import TodoList from "@/components/TodoList.vue";
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    TodoSimpleForm,
+    TodoList
+  },
+  setup() {
+    const todos = ref([]);
+
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    };
+
+    const ToggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed
+    }
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    }
+
+    const searchText = ref("");
+
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter( todo => {
+          return todo.subject.includes(searchText.value)
+        })
+      } 
+      return todos.value
+    })
+
+    return {
+      todos,
+      addTodo,
+      deleteTodo,
+      ToggleTodo,
+      searchText,
+      filteredTodos
+    };
+  },
+};
 </script>
 
 <style>
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
